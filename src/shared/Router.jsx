26ac98edfedf57533c8a97b8d09f1996bb4b882d
miing_gaeng <pem4134@naver.com>
2025-02-20 19:57:37 +0900

    feat: Router 연결 #1

---
+++ src/shared/Router.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import MyPage from "../pages/MyPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/MyPage" element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
