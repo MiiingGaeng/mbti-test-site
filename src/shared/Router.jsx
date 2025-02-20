8f52111a2df0d9ee1699fb2e6b772b257b0e56bb
miing_gaeng <pem4134@naver.com>
2025-02-20 20:08:59 +0900

    add: Layout/Header 파일 생성 및 라우터 추가 #1

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -3,15 +3,18 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import MyPage from "../pages/MyPage";
+import Layout from "../layout/Layout";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/MyPage" element={<MyPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/MyPage" element={<MyPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
