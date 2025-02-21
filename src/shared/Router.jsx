5fb518e70fd43af462882b6a7c0ede346778e3c4
miing_gaeng <pem4134@naver.com>
2025-02-21 20:24:48 +0900

    feat: 로그인 여부에 따른 페이지 이동 로직 추가 #2

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,10 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import MyPage from "../pages/MyPage";
 import Layout from "../layout/Layout";
 import Test from "../pages/Test";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+//로그인이 필요없는 페이지
+const PublicPage = ({ element: Element, ...rest }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+  return !isAuthenticated ? <Element {...rest} /> : <Navigate to="/" />;
+};
+
+//로그인이 필요한 페이지
+const PrivtePage = ({ element: Element, ...rest }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+  return isAuthenticated ? <Element {...rest} /> : <Navigate to="/login" />;
+};
 
 const Router = () => {
   return (
@@ -12,9 +26,9 @@ const Router = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/MyPage" element={<MyPage />} />
+          <Route path="/login" element={<PublicPage element={Login} />} />
+          <Route path="/signup" element={<PublicPage element={Signup} />} />
+          <Route path="/MyPage" element={<PrivtePage element={MyPage} />} />
           <Route path="/test" element={<Test />} />
         </Route>
       </Routes>
