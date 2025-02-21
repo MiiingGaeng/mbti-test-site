249a1d22e30cd7d1e9ec2a30e2dba5f5b9a54326
miing_gaeng <pem4134@naver.com>
2025-02-21 19:59:59 +0900

    feat: 인증 상태에 따른 헤더 메뉴 변경 로직 #2

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,16 +1,44 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Header = () => {
+  //-----context-----
+  const { isAuthenticated, removeTokenInLocal } = useContext(AuthContext);
+  //-----navigate-----
+  const navigate = useNavigate();
+
+  //logout 로직
+  const handleLogOut = () => {
+    //accessToken 삭제
+    removeTokenInLocal();
+    //홈 화면으로 리다이렉션
+    navigate("/");
+  };
+
   return (
-    <div className="flex bg-blue-500">
+    <div className="flex justify-between p-4 bg-indigo-400">
       <h1>MBTI</h1>
-      <div>
-        <Link to="/signup">
-          <button type="button">Sign Up</button>
-        </Link>
-        <Link to="/login">
-          <button type="button">Login</button>
-        </Link>
+      <div className="flex gap-4">
+        {isAuthenticated ? (
+          <Link to="/MyPage">
+            <button type="button">MyPage</button>
+          </Link>
+        ) : (
+          <Link to="/signup">
+            <button type="button">Sign Up</button>
+          </Link>
+        )}
+        {/* 로그인 상태면 로그아웃 버튼, 아니면 로그인 버튼 */}
+        {isAuthenticated ? (
+          <button type="button" onClick={handleLogOut}>
+            Logout
+          </button>
+        ) : (
+          <Link to="/login">
+            <button type="button">Login</button>
+          </Link>
+        )}
       </div>
     </div>
   );
