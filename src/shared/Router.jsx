3bffbde70ea845b12279100e2a10367400576b84
miing_gaeng <pem4134@naver.com>
2025-02-24 16:21:42 +0900

    refactor : Router의 public/privite페이지 리팩토링- zustand #5

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -5,20 +5,19 @@ import Signup from "../pages/Signup";
 import MyPage from "../pages/MyPage";
 import Layout from "../layout/Layout";
 import Test from "../pages/Test";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
 import TestResult from "../pages/TestResult";
+import useLoginStore from "../zustand/loginStore";
 
 //로그인이 필요없는 페이지
 const PublicPage = ({ element: Element, ...rest }) => {
-  const { isAuthenticated } = useContext(AuthContext);
+  const { isAuthenticated } = useLoginStore();
   return !isAuthenticated ? <Element {...rest} /> : <Navigate to="/" />;
 };
 
 //로그인이 필요한 페이지
-const PrivtePage = ({ element: Element, ...rest }) => {
-  const { isAuthenticated } = useContext(AuthContext);
-  return isAuthenticated ? <Element {...rest} /> : <Navigate to="/login" />;
+const PrivitePage = ({ element: Element, ...rest }) => {
+  const { isAuthenticated } = useLoginStore();
+  return !isAuthenticated ? <Element {...rest} /> : <Navigate to="/login" />;
 };
 
 const Router = () => {
@@ -29,7 +28,7 @@ const Router = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<PublicPage element={Login} />} />
           <Route path="/signup" element={<PublicPage element={Signup} />} />
-          <Route path="/mypage" element={<PrivtePage element={MyPage} />} />
+          <Route path="/mypage" element={<PrivitePage element={MyPage} />} />
           <Route path="/test" element={<Test />} />
           <Route path="/results" element={<TestResult />} />
         </Route>
