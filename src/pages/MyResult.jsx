02c114f2a86d800428b1ab8f839f2b3acac94110
miing_gaeng <pem4134@naver.com>
2025-02-25 11:07:53 +0900

    html/css: 로딩 메세지 스타일 추가

--- src/pages/MyResult.jsx
+++ src/pages/MyResult.jsx
@@ -35,11 +35,13 @@ const MyResult = () => {
   });
 
   if (isResultListPending) {
-    return <p>is Loading...</p>;
+    return <p className="text-lg text-indigo-800 text-center">is Loading...</p>;
   }
 
   if (isResultListError) {
-    return <p>There is an error</p>;
+    return (
+      <p className="text-lg text-indigo-800 text-center">There is an error</p>
+    );
   }
 
   const myResult = resultList.findLast((item) => item.userId === userId);
